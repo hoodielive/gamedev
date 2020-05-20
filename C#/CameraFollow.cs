e43f0fab8dca408c56d6a96b88a44e59eb776ef7
hoodielive <hoodielive@gmail.com>
2020-05-19 20:46:33 -0400

    game script

---
+++ C#/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform pumpkinPos;
+    private float zDistance = 7f;
+    private float yDistance = 4f;
+
+    private void Awake()
+    {
+        pumpkinPos = GameObject.Find("Pumpkin").transform;
+    }
+
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x = pumpkinPos.position.y + yDistance;
+        temp.z = pumpkinPos.position.x - zDistance;
+        transform.position = temp;
+
+    }
+}
---
+++ C#/Pumpkin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour
+{
+    public float moveForce = 10f;
+    public Rigidbody myBody;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        GetComponent<Rigidbody>().drag = 3;
+    }
+
+    private void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        MovePumpkin();
+    }
+
+    private void MovePumpkin()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+         myBody.AddForce(new Vector3(h * moveForce, 0f, v * moveForce));
+    }
+}
