c2c785a2ea56002781d6c8172cf1af6b0f525e2f
hoodielive <hoodielive@gmail.com>
2020-05-28 22:20:17 -0400

    Working on a private repo developing my game. Here is some of it

---
+++ C#/UnityScripts/DNA.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    // Gene for color.
+    public float r;
+    public float g;
+    public float b;
+    bool dead = false;
+    public float timeToDie = 0f;
+    private SpriteRenderer _sRenderer;
+    private Collider2D _sCollider;
+
+    private void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        Debug.Log("Dead At: " + timeToDie);
+        _sRenderer.enabled = false;
+        _sCollider.enabled = false;
+    }
+
+    void Start()
+    {
+        _sRenderer = GetComponent<SpriteRenderer>();
+        _sCollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
