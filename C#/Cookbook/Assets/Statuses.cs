a17060459fa37c753ae57eb2b982998239823760
hoodielive <hoodielive@gmail.com>
2020-06-15 21:40:34 -0400

    I am working on rotation, position and transform(s).

---
+++ C#/Cookbook/Assets/Statuses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statuses : MonoBehaviour
+{
+    public Vector3 direction = Vector3.up;
+    private float speed = 0.1f;
+    void Start()
+    {
+       Debug.Log("I am running from Start"); 
+    }
+    private void Awake()
+    {
+        Debug.Log("I am running from Awake");
+    }
+    void LateUpdate()
+    {
+        Debug.Log("I am running for LateUpdate");
+    }
+    void Update()
+    {
+        var movement = direction * speed;
+        movement *= Time.deltaTime;
+        this.transform.Translate(movement);
+    }
+
+}
