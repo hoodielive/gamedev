f63ade6150e47ce5b3854d012e634136e4e767da
hoodielive <hoodielive@gmail.com>
2020-06-18 14:33:56 -0400

    Oh boy..

---
+++ C#/Explosion.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+	public GameObject[] explosionPrefabs;
+	public int amountOfParticles = 3;
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+
+	}
+}
