e12260e82444cb3d4418b013d26f5bc94295a55a
hoodielive <hoodielive@gmail.com>
2020-06-21 23:26:18 -0400

    Love

---
+++ C#/Entities/LetsUnity/Assets/ManipulateCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipulateCube : MonoBehaviour
+{
+   void Start()
+   {
+      foreach (Transform child in transform)
+      {
+         child.position += Vector3.up * 10.0f;
+         Debug.Log("Tell me what is happening.");
+      }
+   }
+}
