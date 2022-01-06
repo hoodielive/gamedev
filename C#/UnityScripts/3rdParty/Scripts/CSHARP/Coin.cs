633cb9ffc5c58055b23a156858649efeb09df442
hoodielive <hoodielive@gmail.com>
2022-01-05 23:51:47 -0500

    Added some scripts from 3rd Parties for Study.

---
+++ C#/UnityScripts/3rdParty/Scripts/CSHARP/Coin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+    private Transform ThisTransform = null;
+    public float RotSpeed = 45f;
+
+    private void Awake()
+    {
+        ThisTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        ThisTransform.Rotate(0,0, RotSpeed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        //Get a list of all coins
+        GameObject[] Coins = GameObject.FindGameObjectsWithTag("Coin");
+
+        Destroy(gameObject);
+
+        if(Coins.Length-1 <= 0)
+        {
+            print("Game Completed!");
+        }
+    }
+}
---
+++ C#/UnityScripts/3rdParty/Scripts/CSHARP/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour 
+{
+    public float TotalTime = 60f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Invoke("RestartLevel", TotalTime);
+	}
+
+    public void RestartLevel()
+    {
+        print("Restart Level");
+    }
+}
