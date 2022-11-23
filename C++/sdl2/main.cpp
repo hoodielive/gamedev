b60399e2a7de702a2761578cd6cf58b50a7373af
hoodielive <hoodielive@gmail.com>
2022-11-23 00:41:52 -0500

    Checking in

---
+++ C++/sdl2/main.cpp
@@ -0,0 +1,32 @@
+#include <SDL2/SDL.h>
+
+int main(int argc, char** argv)
+{
+  bool quit = false;
+  SDL_Event event;
+  
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window* screen = SDL_CreateWindow(
+      "My SDL Empty Window",
+      SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      640,
+      480,
+      0
+  );
+
+  while (!quit)
+  {
+    SDL_WaitEvent(&event);
+    switch (event.type)
+    {
+      case SDL_QUIT:
+        quit = true;
+        break;
+    }
+  }
+  SDL_Quit();
+
+  return 0;
+}
