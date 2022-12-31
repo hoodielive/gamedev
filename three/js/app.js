cea4a0fb4cd8ddd45a97ef0b30246d9c16359211
hoodielive <hoodielive@gmail.com>
2022-12-31 13:01:10 -0500

    Okay Three.js I see you. I cannot seem to get OrbitControls to work here.

--- three/js/app.js
+++ three/js/app.js
@@ -5,19 +5,24 @@
  * 3. Renderer
  */ 
 
-const scene = new THREE.Scene();
+const EGUN = THREE;
+// import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
+
+const scene = new EGUN.Scene();
 
 // field of view, aspect ratio, near and far clipping plane
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const camera = new EGUN.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new EGUN.WebGLRenderer();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const controls = new EGUN.OrbitControls( camera, renderer.domElement );
+
 // Cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const geometry = new EGUN.BoxGeometry( 1, 1, 1 );
+const material = new EGUN.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new EGUN.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
