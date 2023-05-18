2ac8955652195cad49bd3c0d1523d9f7f1a7b477
hoodielive <hoodielive@gmail.com>
2023-05-17 22:25:10 -0400

    End.

---
+++ C++/warmups/func.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+public void bombPlayer(int power, int direction); 
+
+public void shootLazers(int power, int direction);
+
+
+int addAtoB(int a, int b)
+{
+  int answer = a + b;
+  return answer;
+}
+
+void doWhatever();
+void doSomethingCool();
+
+int main()
+{
+  bombPlayer(50, 180);
+}
